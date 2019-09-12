6a977c126244e5aaf2d11567b11e5aed68cdeebd
mtslck <mats.loock@lnu.se>
2019-09-12 09:51:35 +0200

    Updating dependencies

--- app.js
+++ app.js
@@ -9,12 +9,12 @@
 
 const ma = require('./src/my-array')
 
-let arr = [
-  {name: 'Stina', points: 1},
-  {name: 'Erik', points: 2},
-  {name: 'Maja', points: 3},
-  {name: 'Sven', points: 4}
+const arr = [
+  { name: 'Stina', points: 1 },
+  { name: 'Erik', points: 2 },
+  { name: 'Maja', points: 3 },
+  { name: 'Sven', points: 4 }
 ]
 
-let result = ma.filterBetterThanAverage(arr)
+const result = ma.filterBetterThanAverage(arr)
 console.log(result) // ['Maja', 'Sven']
